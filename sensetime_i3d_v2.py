26b33b8d4739e2ccfb17e204738a40f77f4cc80c
zhouyao <zhouyao@sensetime.com>
2018-01-17 10:15:21 +0800

    ddd

--- sensetime_i3d_v2.py
+++ sensetime_i3d_v2.py
@@ -8,28 +8,7 @@ from __future__ import division
 from __future__ import print_function
 
 import tensorflow as tf
-
-def unit3D(inputs, output_channels,
-           kernel_shape=(1, 1, 1),
-           strides=(1, 1, 1),
-           activation_fn=tf.nn.relu,
-           use_batch_norm=True,
-           use_bias=False,
-           padding='same',
-           is_training=True,
-           name=None):
-    """Basic unit containing Conv3D + BatchNorm + non-linearity."""
-    with tf.variable_scope(name, 'unit3D', [inputs]):
-        net = tf.layers.conv3d(inputs, filters=output_channels,
-                            kernel_size=kernel_shape,
-                            strides=strides,
-                            padding=padding,
-                            use_bias=use_bias)
-        if use_batch_norm:
-            net = tf.contrib.layers.batch_norm(net, is_training=is_training)
-        if activation_fn is not None:
-            net = activation_fn(net)
-    return net
+from net_utils import unit3D
 
 def SenseTime_I3D_V2(inputs, is_training=True,
                      final_endpoint='Prediction',
